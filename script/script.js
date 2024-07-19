1066263b12ce0bf12df5a05346a35a7faa98784b
Nikita <n1k_1ta@bk.ru>
2024-07-19 17:40:38 +0500

    test-commit

---
+++ script/script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const productSelect = document.getElementById('product');
+    const specificTypeContainer = document.getElementById('specificTypeContainer');
+
+    productSelect.addEventListener('change', function() {
+        if (productSelect.value === 'appliance') {
+            specificTypeContainer.style.display = 'block';
+        } else {
+            specificTypeContainer.style.display = 'none';
+        }
+    });
+
+    const animatingText = document.querySelectorAll('.animating-text span');
+    let currentIndex = 0;
+
+    function showText(index) {
+        animatingText[index].classList.add('show');
+    }
+
+    function cycleText() {
+        if (currentIndex < animatingText.length) {
+            showText(currentIndex);
+            currentIndex++;
+            setTimeout(cycleText, 500);
+        }
+    }
+
+    cycleText();
+
+    const menuIcon = document.getElementById('menu-icon');
+    const navLinks = document.getElementById('nav-links');
+
+    menuIcon.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
+
+
+
---
+++ script/server.mjs
@@ -0,0 +1,86 @@
+import express from 'express';
+import multer from 'multer';
+import fetch from 'node-fetch';
+import fs from 'fs';
+import FormData from 'form-data';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+const upload = multer({ dest: 'uploads/' });
+
+app.use(cors({
+    origin: 'http://127.0.0.1:5500/main.html' // Замените этот адрес на ваш фактический
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/style', express.static(path.join(path.resolve(), 'style')));
+app.use('/script', express.static(path.join(path.resolve(), 'script')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(path.resolve(), 'main.html'));
+});
+
+app.get('/confirmation', (req, res) => {
+    res.sendFile(path.join(path.resolve(), 'confirmation.html'));
+});
+
+app.post('/submit-form', upload.single('image'), async (req, res) => {
+    console.log('Received form submission');
+    const { name, phone, product, model, condition, price, description, specificType } = req.body;
+    const image = req.file;
+
+    console.log('Form data:', { name, phone, product, model, condition, price, description, specificType });
+    console.log('Image file:', image);
+
+    const botToken = '7011778898:AAH1Ych5EKziMaaXKhRkUgmJDaX4UEkLp0Q';
+    const chatId = '-4174297101';
+
+    const message = `
+    Имя: ${name}
+    Телефон: ${phone}
+    Категория товара: ${product}${specificType ? ' (' + specificType + ')' : ''}
+    Модель: ${model}
+    Состояние: ${condition}
+    Цена: ${price}
+    Описание: ${description}
+    `;
+
+    try {
+        console.log('Отправка сообщения...');
+        const response = await fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ chat_id: chatId, text: message })
+        });
+        const data = await response.json();
+        console.log('Результат отправки сообщения:', data);
+
+        if (response.ok && image) {
+            console.log('Отправка фото...');
+            const form = new FormData();
+            form.append('chat_id', chatId);
+            form.append('photo', fs.createReadStream(image.path));
+
+            const photoResponse = await fetch(`https://api.telegram.org/bot${botToken}/sendPhoto`, {
+                method: 'POST',
+                body: form
+            });
+            const photoData = await photoResponse.json();
+            console.log('Результат отправки фото:', photoData);
+        }
+
+        res.redirect('/confirmation');
+    } catch (error) {
+        console.error('Ошибка:', error);
+        res.status(500).send('Не удалось отправить форму');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Сервер запущен на http://localhost:${port}/`);
+});
